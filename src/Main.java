65a7540aac9bd00c5c770b41464091291e0e3329
滚雪球 <42798930+siknight@users.noreply.github.com>
2020-11-03 12:54:18 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("local");
         System.out.println("remote!");
+         System.out.println("11111!");
     }
 }
